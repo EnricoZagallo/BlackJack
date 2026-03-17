54eaa51a2fdc916015d7a118c2350c5eb82ee3d5
Enrico Zagallo <enricozagallo92@gmail.com>
2026-03-17 18:07:16 -0300

    first commit

--- src/model/Deck.java
+++ src/model/Deck.java
@@ -1,4 +1,36 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Deck {
+    String[] suits = {"Hearts", "Diamonds", "Spades", "Clubs"};
+    String[] values = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+    List<String> valuesList = new ArrayList<>();
+    List<String> deckShuflled = new ArrayList<>();
+
+    public void gerarBaralho() {
+
+
+        for (String suit : suits) {
+            for (String value : values) {
+                valuesList.add(value + " of " + suit);
+            }
+        }
+        for (String c : valuesList) {
+            System.out.println(c);
+        }
+    }
+
+    public void Shuffle() {
+        Collections.shuffle(valuesList);
+        for (String c : valuesList) {
+            System.out.println(c);
+        }
+    }
+
+    public String getCard() {
+        return valuesList.remove(0);
+    }
 }
--- src/model/Player.java
+++ src/model/Player.java
@@ -1,4 +1,16 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
+    List<String> playerHand = new ArrayList<>(2);
+
+    public void receiveCard(String card){
+        playerHand.add(card);
+    }
+    public List<String> getPlayerHand() {
+        return playerHand;
+
+    }
 }
